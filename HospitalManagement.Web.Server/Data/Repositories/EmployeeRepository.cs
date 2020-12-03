fd2217cc2567e106bd8fa10bc3ec9be1e560664a
Kipoderax <sebastian.rajczyk@wp.pl>
2020-12-03 23:35:17 +0100

    Add Employee repository

---
+++ HospitalManagement.Web.Server/Data/Repositories/EmployeeRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Web.Server
+{
+    /// <summary>
+    /// Management employees 
+    /// </summary>
+    public class EmployeeRepository : GenericRepository, IEmployeeRepository
+    {
+        #region Private Members
+
+        /// <summary>
+        /// data context based on database
+        /// </summary>
+        private readonly DataContext _dataContext;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="dataContext">Injected data context</param>
+        public EmployeeRepository (DataContext dataContext) : base(dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        #endregion
+
+        #region Implemented Methods
+
+        /// <summary>
+        /// Get list of all employees
+        /// </summary>
+        /// <returns>All employees</returns>
+        public async Task<IEnumerable<Employee>> GetEmployees ()
+        {
+            var employees = await _dataContext.Employees.ToListAsync();
+
+            return employees;
+        }
+
+        #endregion
+    }
+}
---
+++ HospitalManagement.Web.Server/Data/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Web.Server
+{
+    /// <summary>
+    /// Management employees like show list, get specify employee and so on
+    /// </summary>
+    public interface IEmployeeRepository
+    {
+        /// <summary>
+        /// Get list of all employees
+        /// </summary>
+        /// <returns>All employees</returns>
+        Task<IEnumerable<Employee>> GetEmployees ();
+    }
+}
