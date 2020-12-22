c4f398be1f2ac926ab7ce5da41c62a2a54a81d1d
Kipoderax <sebastian.rajczyk@wp.pl>
2020-12-22 14:27:53 +0100

    Fill user panel content with personal details and personal duty list

---
+++ HospitalManagement/Controls/Duty/DutyPersonalListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HospitalManagement
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DutyPersonalListControl.xaml
+    /// </summary>
+    public partial class DutyPersonalListControl : UserControl
+    {
+        public DutyPersonalListControl ()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HospitalManagement/Controls/Duty/DutyPersonalListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HospitalManagement
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DutyPersonalListItemControl.xaml
+    /// </summary>
+    public partial class DutyPersonalListItemControl : UserControl
+    {
+        public DutyPersonalListItemControl ()
+        {
+            InitializeComponent();
+        }
+    }
+}
