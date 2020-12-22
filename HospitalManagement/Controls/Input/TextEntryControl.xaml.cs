eff403f01b7f3d921155878de834b05169ac30e7
Kipoderax <sebastian.rajczyk@wp.pl>
2020-12-22 17:48:32 +0100

    Prepare user personal control details inside user settings

---
+++ HospitalManagement/Controls/Input/TextEntryControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace HospitalManagement
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TextEntryControl.xaml
+    /// </summary>
+    public partial class TextEntryControl : UserControl
+    {
+        public TextEntryControl ()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HospitalManagement/ValueConverters/BooleanInvertConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace HospitalManagement
+{
+    /// <summary>
+    /// A converter that takes in a boolean and inverts it
+    /// </summary>
+    public class BooleanInvertConverter : BaseValueConverter<BooleanInvertConverter>
+    {
+        public override object Convert ( object value, Type targetType, object parameter, CultureInfo culture ) => !(bool) value;
+    }
+}
