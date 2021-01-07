cf74458fa6190606bf79702610a90deab154708e
Kipoderax <sebastian.rajczyk@wp.pl>
2021-01-07 23:00:22 +0100

    Design calendar custom UI view

---
+++ HospitalManagement.Core/ViewModel/Duty/DutyCalendarViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace HospitalManagement.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DutyCalendarViewModel
+    {
+        // TODO: It's place to colaborate with duty calendar control
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public DutyCalendarViewModel ()
+        {
+
+        }
+
+        #endregion
+    }
+}
